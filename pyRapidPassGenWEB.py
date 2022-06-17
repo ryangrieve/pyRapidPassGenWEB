215f95e01a2515fc559d52bd04562df9b4260b42
Ryan Grieve <66217567+ryangrieve@users.noreply.github.com>
2022-06-17 10:48:41 -0600

    Refactoring

--- pyRapidPassGenWEB.py
+++ pyRapidPassGenWEB.py
@@ -17,7 +17,7 @@ st.subheader("A simple password generator using python.")
 col1, col2 = st.columns(2)
 
 with col1:
-    char, number_pass, special_chars = (
+    chars, number_pass, special_chars = (
         st.slider("Preferred password length?", min_value=16, max_value=50),
         st.slider("Number of passwords required?", min_value=1, max_value=25),
         st.radio("Use special characters?", ["yes", "no"]),
@@ -30,10 +30,6 @@ with col1:
             st.success("pyRapidPassGen task completed!")
             for i in range(number_pass):
                 CHARACTERS = (ascii_letters, digits, punctuation)
-                if special_chars == "yes":
-                    x = [CHARACTERS[i] for i in [0, 1, 2]]
-                    password = "".join(choice("".join(x)) for i in range(char))
-                elif special_chars == "no":
-                    x = [CHARACTERS[i] for i in [0, 1]]
-                    password = "".join(choice("".join(x)) for i in range(char))
+                x = CHARACTERS if special_chars == "yes" else CHARACTERS[0:1]
+                password = "".join(choice("".join(x)) for i in range(chars))
                 st.text(password)
